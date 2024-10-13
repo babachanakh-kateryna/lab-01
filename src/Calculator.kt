3d7f102dfc40c490e6d33bf52bdff7e6f14f8d5e
Kateryna <kateryna.babachanakh1@etu.univ-lorraine.fr>
2024-10-13 23:20:58 +0200

    Add the main function that runs the calculator

---
+++ src/Calculator.kt
@@ -0,0 +1,45 @@
+import java.util.Scanner
+
+/**
+ * This is the main function that runs the calculator.
+ */
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    while (true) {
+        println("Choose operation:")
+        println("1. Adding (+)")
+        println("2. Subtraction (-)")
+        println("3. Multiplication (*)")
+        println("4. Division (/)")
+        println("5. Conversion of lengths (m, cm, mm)") // individual task - Option 1
+        println("6. Calculation of the volume of a cube") // individual task - Option 8
+        println("7. Exit")
+
+        when (scanner.nextInt()) {
+            1 -> {
+                // TO DO
+            }
+            2 -> {
+                // TO DO
+            }
+            3 -> {
+                // TO DO
+            }
+            4 -> {
+                // TO DO
+            }
+            5 -> {
+                // TO DO
+            }
+            6 -> {
+                // TO DO
+            }
+            7 -> {
+                println("Exit the program...")
+                break
+            }
+            else -> println("Wrong choice. Try again.")
+        }
+    }
+}
